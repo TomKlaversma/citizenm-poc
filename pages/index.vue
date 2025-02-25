97dc950387dde49e7e9f38de6f6c520dcf79c4dd
Tom Klaversma <tomklaversma@gmail.com>
2025-02-25 22:05:33 +0100

    Added container and styled pages/index

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,28 @@
 <template>
-  <div>
-    <h1>Welcome to the homepage</h1>
+  <div class="container">
+    <h1>{{ offersCount }} available hotels in Europe</h1>
     <CurrencySelect />
 
-    <div class="">Grid here</div>
+    <div class="hotel-cards">
+      <HotelCard
+        v-for="hotel in offers?.data"
+        :key="hotel.name"
+        :hotel="hotel"
+      />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 const { data: offers } = await useFetch("/api/offers");
+
+const offersCount = computed(() => offers.value?.data.length);
 </script>
+
+<style scoped lang="scss">
+.hotel-cards {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 2rem;
+}
+</style>
