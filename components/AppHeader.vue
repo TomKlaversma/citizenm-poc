086439bc4f30680d8f47ee9fba4e75331ca90998
Tom Klaversma <tomklaversma@gmail.com>
2025-02-25 21:00:54 +0100

    Added header

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,3 +1,35 @@
 <template>
-  <nav>hi</nav>
+  <nav>
+    <ul>
+      <li>
+        <NuxtLink to="/">
+          <img src="/assets/img/logo.svg" alt="logo" />
+        </NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/#"> browse hotels by city </NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/#"> book a meeting room </NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/#"> membership </NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/#"> offers </NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/#"> about us </NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/#"> FAQ </NuxtLink>
+      </li>
+    </ul>
+    <ul class="main-nav__aside">
+      <li>( / )</li>
+      <li>
+        <NuxtLink to="/#"> Hi, John Doe </NuxtLink>
+      </li>
+    </ul>
+  </nav>
 </template>
