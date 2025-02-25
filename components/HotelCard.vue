68062627cb0dcc4a70eb94129e2b4003969b8808
Tom Klaversma <tomklaversma@gmail.com>
2025-02-25 22:23:11 +0100

    Added card basis

--- components/HotelCard.vue
+++ components/HotelCard.vue
@@ -4,9 +4,11 @@
     <div class="hotel-card__available-label">Available</div>
     <div class="hotel-card__content">
       <h2 class="hotel-card__name">{{ hotel.name }}</h2>
-      <p class="hotel-card__description">{{ hotel.description }}</p>
-      <p class="hotel-card__currency">{{ hotel.locale.currency }}</p>
-      <p class="hotel-card__offers">{{ hotel.offers }}</p>
+      <div class="hotel-card__lowest-offer">
+        <span>rooms available from</span>
+        <span class="hotel-card__lowest-offer-price">{{ lowestOffer }}</span>
+      </div>
+      <a href="#" class="hotel-card__cta">view rates</a>
     </div>
   </div>
 </template>
@@ -20,10 +22,11 @@ import image_5 from "~/assets/img/rome.webp";
 import image_6 from "~/assets/img/rotterdam.jpg";
 import image_7 from "~/assets/img/zurich.jpg";
 
-defineProps<{
+const props = defineProps<{
   hotel: Hotel;
 }>();
 
+// Grab a random image from the images array
 const randomImage = computed(() => {
   const images = [
     image_1,
@@ -37,6 +40,14 @@ const randomImage = computed(() => {
 
   return images[Math.floor(Math.random() * images.length)];
 });
+
+// Grab the lowest offer price from the hotel offers array
+const lowestOffer = computed(() =>
+  props.hotel.offers.reduce(
+    (lowest, offer) => Math.min(lowest, offer.price),
+    props.hotel.offers[0].price
+  )
+);
 </script>
 
 <style scoped lang="scss">
@@ -44,15 +55,22 @@ const randomImage = computed(() => {
   position: relative;
   display: flex;
   flex-direction: column;
-  gap: 2rem;
-  border: 1px solid #ccc;
+  border: 1px solid var(--border);
+  border-radius: 0.25rem;
+
+  &__image {
+    width: 100%;
+    object-fit: cover;
+    object-position: center;
+    aspect-ratio: 16 / 9;
+  }
 
   &__available-label {
     position: absolute;
     top: 1rem;
     left: 1rem;
-    background-color: rgb(15, 192, 127);
-    color: #fff;
+    background-color: var(--secondary-green);
+    color: var(--primary-white);
     padding: 0.125rem 0.5rem;
     font-size: 0.875rem;
     border-radius: 0.25rem;
@@ -60,6 +78,36 @@ const randomImage = computed(() => {
 
   &__content {
     padding: 1rem;
+  }
+
+  &__lowest-offer {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    font-weight: 400;
+    justify-content: space-between;
+    font-size: 0.875rem;
+    background: var(--light-gray-surface);
+    padding: 0.5rem 1rem;
+    border-radius: 0.25rem;
+  }
+
+  &__lowest-offer-price {
+    font-size: 1.5rem;
+  }
+
+  &__cta {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    padding: 1rem 1rem;
+    background-color: var(--primary-red);
+    color: var(--primary-white);
+    border-radius: 0.25rem;
+    text-decoration: none;
+    font-weight: 700;
+    font-size: 1.25rem;
   }
 }
 </style>
