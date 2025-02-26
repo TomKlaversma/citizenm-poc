721fa1bef50908bb331cfa2f4429ffe41164f3d3
Tom Klaversma <tomklaversma@gmail.com>
2025-02-26 16:29:18 +0100

    Simplified server

--- server/api/exchange-rate.ts
+++ server/api/exchange-rate.ts
@@ -1,8 +1,5 @@
 import data from "./data/exchange-rate.json";
 
 export default defineEventHandler(() => {
-  return {
-    success: true,
-    data,
-  };
+  return data;
 });
--- server/api/offers.ts
+++ server/api/offers.ts
@@ -1,8 +1,5 @@
 import data from "./data/offers.json";
 
 export default defineEventHandler(() => {
-  return {
-    success: true,
-    data,
-  };
+  return data;
 });
